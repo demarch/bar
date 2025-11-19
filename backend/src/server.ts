927b3110244e1177aba219d7a80a6181aa376e74
Pedro Augusto <pachete2014@gmai.com>
2025-11-19 16:41:28 -0300

    feat: Increase API rate limit to 1000 maximum requests.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -53,7 +53,7 @@ const io = new SocketServer(server, {
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 1000, // limit each IP to 1000 requests per windowMs
   message: 'Muitas requisições deste IP, tente novamente mais tarde.',
 });
 
